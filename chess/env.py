808168b6c3679a76223a3bc457972e663e17d45e
Pedro Jorge <pmrjge@gmail.com>
2022-06-28 18:07:23 +0100

    Initial project setup w/ .gitignore

---
+++ chess/env.py
@@ -0,0 +1,36 @@
+from typing import Any, Tuple, TypeVar
+import chex
+import pax
+import jax.numpy as jnp
+import jax
+import jax.random as jr
+
+E = TypeVar("E")
+
+class Environment(pax.Module):
+    def __init__(self):
+        super().__init__()
+
+    def step(self: E, action: chex.Array) -> Tuple[E, chex.Array]:
+        raise NotImplementedError()
+
+    def reset(self):
+        pass
+
+    def is_terminated(self) -> chex.Array:
+        raise NotImplementedError()
+
+    def observation(self) -> Any:
+        pass
+
+    def canonical_observation(self) -> Any:
+        pass
+
+    def num_actions(self) -> int:
+        raise NotImplementedError()
+
+    def invalid_actions(self) -> chex.Array:
+        raise NotImplementedError()
+
+    def max_num_steps(self) -> int:
+        raise NotImplementedError()
