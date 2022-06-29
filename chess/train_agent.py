9c35cbe8a4a94a7a09116c889130352e83a25741
Pedro Jorge <pmrjge@gmail.com>
2022-06-29 02:13:02 +0100

    Tested training with different parameters to speed up code bug analysis

--- chess/train_agent.py
+++ chess/train_agent.py
@@ -42,6 +42,7 @@ class MoveOutput:
     action_weights: chex.Array
 
 
+@partial(jax.jit, static_argnums=(3, 4, 5))
 @partial(jax.pmap, in_axes=(None, None, 0), static_broadcasted_argnums=(3, 4, 5))
 def collect_batched_self_play_data(agent, env: Environment, rng_key: chex.Array, batch_size: int, num_simulations_per_move: int, temperature_decay: float):
 
@@ -125,7 +126,7 @@ def train_step(net, optim, data: TrainingExample):
     return net, optim, losses
 
 
-def train(game_class="chess_game.ChessGame", agent_class="ResNetPolicy.ResnetPolicyValueNet", batch_size: int = 64,
+def train(game_class="chess_game.ChessGame", agent_class="ResNetPolicy.ResnetPolicyValueNet", batch_size: int = 128,
           num_iterations: int = 256, num_simulations_per_move: int = 2048, num_self_plays_per_interaction: int = 4096,
           learning_rate: float = 0.001, ckpt_filename: str = "./data/agent.ckpt", random_seed: int = 88, weight_decay: float = 1e-4,
           temperature_decay=0.9, buffer_size: int = 66_666, rng_key=None):
@@ -188,6 +189,6 @@ def train(game_class="chess_game.ChessGame", agent_class="ResNetPolicy.ResnetPol
 if __name__ == '__main__':
     print("Cores :::: ", jax.local_devices())
 
-    train = partial(train, game_class="test_connect_four.Connect4Game")
+    train = partial(train, game_class="test_connect_four.Connect4Game", num_iterations=512, num_simulations_per_move=64, num_self_plays_per_interaction=2048)
 
     fire.Fire(train)
